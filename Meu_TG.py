723c1e55f851d8ce2f6266b27d308ba5117e8222
DaniloRocha077 <84972544+DaniloRocha077@users.noreply.github.com>
2023-05-22 21:26:16 -0300

    Add files via upload

---
+++ Meu_TG.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+# configurações gerais da página
+st.set_page_config(page_title="Meu TCC", page_icon="📄")
+
+
+st.markdown("# 🎓 O que é o **Trabalho de Graduação (TG)**?")
+st.markdown("O Trabalho de Graduação, ou **TG**, é um desafio emocionante que todo estudante universitário deve enfrentar. Ele é um trabalho científico que você deve escrever para mostrar que sabe muito sobre um tópico específico relacionado ao seu curso. O TG é a chance de ouro para você juntar tudo que aprendeu durante sua graduação e mostrar para o mundo o que sabe! 😎")
+st.markdown("---")
+
+st.markdown("### 🤔 Por que o TG é tão importante?")
+st.markdown("O TG é um requisito *essencial* para que você possa concluir seu curso universitário. Sem ele, você não pode se formar e receber seu diploma (😱). Além disso, ele é uma forma de avaliação para medir o seu conhecimento e habilidades adquiridos durante a graduação. ")
+st.markdown("---")
+
+st.markdown("### 📝 Como desenvolver um TG?")
+st.markdown("Para começar, você precisa identificar um tema interessante e relevante para o seu curso. Depois, deve escrever um **projeto** que defina seu objetivo e metodologia de pesquisa. A partir disso, você deve ler muito, realizar pesquisas bibliográficas, coletar dados e analisar os resultados. Ah, e não se esqueça: tudo isso sob orientação e avaliação de um professor (🤓).")
+st.markdown("---")
+
+st.markdown("### 💪 O que o TG pode fazer por mim?")
+st.markdown("Desenvolver um TG é uma oportunidade incrível para você aprimorar suas habilidades de **análise crítica**, **síntese**, **comunicação escrita e oral**, **trabalho em equipe** e **gerenciamento de tempo**. Você também pode usar seu TG como um grande destaque em seu currículo, mostrando a futuros empregadores que você é um estudante dedicado e capaz. ")
+st.markdown("---")
+
+st.markdown("### 🎉 Como encarar o TG?")
+st.markdown("Encare o TG como um desafio empolgante que você pode superar com sucesso! **Dedique-se ao máximo** para obter um resultado de **excelência**. Lembre-se de que o TG é uma forma de demonstrar todo o seu potencial como estudante universitário. Além disso, você pode aproveitar para incluir um pouco de sua personalidade no trabalho (😜). ")
+st.markdown("---")
+
+st.markdown("Esperamos ter ajudado! Boa sorte na elaboração do seu TG! 🤞")
---
+++ teste.py
@@ -0,0 +1,12 @@
+import re
+import streamlit as st
+import docx
+import pydantic
+import connection.database as condb
+import time
+import pydantic
+
+print("Versão do pandas> %s" % re._version_)
+print("Versão do pandas> %s" % docx._version_)
+print("Versão do pandas> %s" % time._version_)
+print("Versão do pandas> %s" % pydantic._version_)
